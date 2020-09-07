9335042a23e8cf88fbb51d2a1ce781419c016e0d
wintus <6386129+wintus@users.noreply.github.com>
2020-09-08 06:30:09 +0900

    Solve 4.4

---
+++ src/4-4.ts
@@ -0,0 +1,16 @@
+export type PartiallyPartial<T, K extends keyof T> = Partial<Pick<T, K>> &
+  Pick<T, Exclude<keyof T, K>>;
+
+// 使用例
+
+// 元のデータ
+interface Data {
+  foo: number;
+  bar: string;
+  baz: string;
+}
+
+/*
+ * T1は { foo?: number; bar?: string; baz: string } 型
+ */
+type T1 = PartiallyPartial<Data, "foo" | "bar">;
